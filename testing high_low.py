f1000a1971154a615ef759b7fb10efce0b4f00ed
ERColaizzi <ercolaizzi@gmail.com>
2024-12-02 11:46:36 -0500

    Update testing high_low.py

--- testing high_low.py
+++ testing high_low.py
@@ -1,12 +1,36 @@
 #init checking 
-
+#high low card simulator
+#tie to user
 
 import random
 
-def card()
+def card():
     card=random.randint(1,13)
     return card
-    
-user_card,comp_card = card()
-print(user_card,comp_card)
 
+def result(user,comp,bid):
+    if user==comp:
+        note="Win"
+    
+    elif bid=="high" and user >comp:
+        note="Win"
+    elif bid=="low" and user <comp:
+        note="Win"
+    
+    else:
+        note="Lose"
+    return note
+
+
+#main    
+while True:
+    user_card,comp_card = card(),card()
+    print("user:",user_card,"comp:",comp_card)
+
+    user_bid = input("high or low: ")
+    print(result(user_card,comp_card,user_bid))
+    repeat = input("play again?")
+    if repeat in ("x","n"):
+        break
+    else:
+        True
