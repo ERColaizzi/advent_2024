5409650217f9479539c2a3ad803f6a167f093287
ERColaizzi <ercolaizzi@gmail.com>
2024-12-02 11:31:37 -0500

    init

---
+++ day1/day1.py
@@ -0,0 +1,2 @@
+# init statement here
+#
---
+++ testing high_low.py
@@ -0,0 +1,12 @@
+#init checking 
+
+
+import random
+
+def card()
+    card=random.randint(1,13)
+    return card
+    
+user_card,comp_card = card()
+print(user_card,comp_card)
+
