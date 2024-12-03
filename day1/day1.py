972e7e8a538c647122189662a52ba5eab9771d84
Ethan R Colaizzi <ercolaizzi@gmail.com>
2024-12-02 21:07:11 -0500

    push

--- day1/day1.py
+++ day1/day1.py
@@ -1,2 +1,23 @@
 # init statement here
-#
+#day 1 part 1
+
+
+#thoughts
+#split lines
+#create two listss
+#order lists
+# for i in L1/L2 compare
+#distance is abs from L1 to L2 and add to running total 
+
+list1=[]
+list2=[]
+
+with open('C:\Users\ercol\Documents\programming\advent_2024\day1\input.txt', 'r') as file:
+    for line in file:
+        list1.append(listeline.split()[0])
+        list2.append(line.split()[1])
+
+print(list1,list2)
+
+
+
